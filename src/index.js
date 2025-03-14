d32e5ea84591652211d2c35cd486e859ff29dc95
Rue-chelle <misshie21@gmail.com>
2025-03-14 09:57:10 +0200

    added basic form handling and basic js with typewritter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "nb mjgjhjmgfhg",
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
